ba6bc43c770acee6c30cd176af0af79fb4fdb986
Najmul190 <68229089+Najmul190@users.noreply.github.com>
2023-05-20 21:48:30 +0100

    Create setup.py for repl.it users

---
+++ setup.py
@@ -0,0 +1,16 @@
+# this file will use os commands to setup the install dependencies and then run main.py (used for repl.it hosting)
+
+import os
+
+# install dependencies
+os.system("pip uninstall discord -y")
+os.system("pip uninstall discord.py -y")
+os.system("pip install -r requirements.txt")
+os.system("pip install -U discord.py-self -y")
+
+# run main.py
+
+os.system("python3 main.py")
+
+print("Finished installing dependencies.")
+print("Make sure to fill out the bot secrets.")
