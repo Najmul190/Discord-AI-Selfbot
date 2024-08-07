4ca373bf44637aa944adadfd6d4625e2d06d7e6a
Najmul190 <68229089+Najmul190@users.noreply.github.com>
2024-08-07 19:19:30 +0100

    Added ChatGPT API

--- utils/ai.py
+++ utils/ai.py
@@ -1,17 +1,27 @@
 from groq import AsyncGroq
+from openai import AsyncOpenAI as OpenAI
 from os import getenv
 from dotenv import load_dotenv
+from sys import exit
 
 load_dotenv(dotenv_path="config/.env")
 
-client = AsyncGroq(api_key=getenv("GROQ_API_KEY"))
+if getenv("OPENAI_API_KEY"):
+    client = OpenAI(api_key=getenv("OPENAI_API_KEY"))
+    model = "gpt-4o"  # "gpt-4o-mini" for cheaper model
+elif getenv("GROQ_API_KEY"):
+    client = AsyncGroq(api_key=getenv("GROQ_API_KEY"))
+    model = "llama3-groq-70b-8192-tool-use-preview"
+else:
+    print("No API keys found, exiting.")
+    exit(1)
 
 
 async def generate_response(prompt, instructions, history=None):
     try:
         if history:
             response = await client.chat.completions.create(
-                model="llama3-groq-70b-8192-tool-use-preview",
+                model=model,
                 messages=[
                     {"role": "system", "content": instructions},
                     {"role": "user", "content": prompt},
@@ -20,7 +30,7 @@ async def generate_response(prompt, instructions, history=None):
             )
         else:
             response = await client.chat.completions.create(
-                model="llama3-groq-70b-8192-tool-use-preview",
+                model=model,
                 messages=[
                     {"role": "system", "content": instructions},
                     {"role": "user", "content": prompt},
